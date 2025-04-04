8ca0e6ceaa65186a0c5d67f93e6d95572843ba7e
George Burgess IV <gbiv@google.com>
2025-04-04 10:51:06 -0600

    llvm_next: add patch for rust/gcc
    These are known to break; add a patch for them so the failures can be
    worked around while they're investigated
    
    BUG=b:408234206, b:408239468
    TEST=None
    
    Change-Id: I1491706d5722b61064cbc8e249be748b938c4d16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/6433725
    Auto-Submit: George Burgess <gbiv@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Reviewed-by: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Tested-by: George Burgess <gbiv@chromium.org>

--- llvm_tools/llvm_next.py
+++ llvm_tools/llvm_next.py
@@ -19,8 +19,9 @@ LLVM_NEXT_REV = 563880
 # llvm_next_py_autoupdate.py.
 # pylint: disable=line-too-long
 LLVM_NEXT_TESTING_CL_URLS: Iterable[str] = (
-    "crrev.com/c/6417962/1",
+    "https://crrev.com/c/6417962/1",
     "https://crrev.com/i/8154895/4",
+    "https://crrev.com/c/6434560/3",
 )
 
 # A list of CLs that constitute the current llvm-next roll.
