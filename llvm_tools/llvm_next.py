7da287cc5fcde3ac7f867aa23d533284ad2932c8
mobiletc-prebuild <mobiletc-prebuild@google.com>
2025-04-17 06:00:08 -0700

    llvm_tools: autoupdate CL list
    - https://crrev.com/i/8154895/15 had a patch-set update
    
    BUG=None
    TEST=CQ+1
    
    Change-Id: I54313c8a8131c02657b78df0bfac5fbcfc753624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/6469352
    Reviewed-by: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Commit-Queue: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Auto-Submit: mobiletc-prebuild Role Account <mobiletc-prebuild@google.com>
    Reviewed-by: George Burgess <gbiv@chromium.org>
    Tested-by: mobiletc-prebuild Role Account <mobiletc-prebuild@google.com>

--- llvm_tools/llvm_next.py
+++ llvm_tools/llvm_next.py
@@ -20,7 +20,7 @@ LLVM_NEXT_REV = 563880
 # pylint: disable=line-too-long
 LLVM_NEXT_TESTING_CL_URLS: Iterable[str] = (
     "https://crrev.com/c/6417962/2",
-    "https://crrev.com/i/8154895/15",
+    "https://crrev.com/i/8154895/16",
 )
 
 # A list of CLs that constitute the current llvm-next roll.
