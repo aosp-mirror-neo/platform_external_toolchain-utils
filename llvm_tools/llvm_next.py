b19e8a2d815b4792c749cf94471455f2a57bc416
Bob Haarman <inglorion@chromium.org>
2025-07-22 23:51:29 +0000

    llvm_tools: update llvm_next.py for LLVM r574158
    BUG=b:432733302
    TEST=llvm_next_test.py
    
    Change-Id: I2cde804b3850d0e506b0472afcb633da8bb55136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/6779621
    Commit-Queue: Bob Haarman <inglorion@chromium.org>
    Reviewed-by: George Burgess <gbiv@chromium.org>
    Tested-by: Bob Haarman <inglorion@chromium.org>
    Reviewed-by: Jordan Abrahams-Whitehead <ajordanr@google.com>

--- llvm_tools/llvm_next.py
+++ llvm_tools/llvm_next.py
@@ -9,8 +9,8 @@ from typing import Iterable
 from llvm_tools import cros_cls
 
 
-LLVM_NEXT_HASH = "386af4a5c64ab75eaee2448dc38f2e34a40bfed0"
-LLVM_NEXT_REV = 563880
+LLVM_NEXT_HASH = "2d287f51eff2a5fbf84458a33f7fb2493cf67965"
+LLVM_NEXT_REV = 574158
 
 # NOTE: Always specify patch-sets for CLs. We don't want uploads by untrusted
 # users to turn into bot invocations w/ untrusted input.
@@ -18,7 +18,10 @@ LLVM_NEXT_REV = 563880
 # Please note that these are (somewhat) automatically curated. See
 # llvm_next_py_autoupdate.py.
 # pylint: disable=line-too-long
-LLVM_NEXT_TESTING_CL_URLS: Iterable[str] = ()
+LLVM_NEXT_TESTING_CL_URLS: Iterable[str] = (
+    "crrev.com/i/8443637/1",
+    "crrev.com/c/6771414/1",
+)
 
 # A list of CLs that constitute the current llvm-next roll.
 # This is taken as the set of CLs that will be landed simultaneously in order
