55ee982b9a3c31b85276d715ac2b163c5385d888
George Burgess IV <gbiv@google.com>
2026-04-22 11:21:11 -0600

    venv wrappers: fix short-lived TODO
    Now that `${script}.py` wrappers have been deleted, we don't need to
    conditionally add `.py`.
    
    BUG=b:505362208
    TEST=repo upload
    
    Change-Id: Ieefdc28aa08f622f5cf04e23b8dbff618615dc6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/7785841
    Tested-by: George Burgess <gbiv@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Reviewed-by: Bob Haarman <inglorion@chromium.org>

--- venvless_python3_wrapper.py
+++ venvless_python3_wrapper.py
@@ -41,15 +41,9 @@ def find_file_to_execute(argv0: str) -> tuple[Path, Path]:
         raise ValueError(
             f"Expected argv0 to be in {prefix} - it's {relative_script_path}"
         )
-    target_script = relative_script_path_str[len(prefix) :]
+    target_script = relative_script_path_str[len(prefix) :] + ".py"
     result = toolchain_utils / target_script
     if not result.exists():
-        # TODO(b/505362208): Unconditionally add .py once the .py symlinks
-        # no longer exist
-        if not result.name.endswith(".py"):
-            result_py = result.with_suffix(".py")
-            if result_py.exists():
-                return result_py, toolchain_utils
         sys.exit(f"No script found at {target_script} - can't execute")
     return result, toolchain_utils
 
