e7051b2f123f92ee9f833138b81bb6b60715c174
George Burgess IV <gbiv@google.com>
2026-04-22 10:42:56 -0600

    add disclaimers to py/bin wrappers
    This should help redirect people/tools that read these files to the
    place they're looking for.
    
    BUG=b:505362208
    TEST=repo upload
    
    Change-Id: Ibb9627e4457ab457753f66c4daf5df751dadba4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/7786689
    Tested-by: George Burgess <gbiv@chromium.org>
    Reviewed-by: Bob Haarman <inglorion@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>

--- venv_tc/venv_python3_wrapper.sh
+++ venv_tc/venv_python3_wrapper.sh
@@ -3,6 +3,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
+# **NOTE**: If you opened a file in py/bin/... and got here, this is unlikely
+# the droid you're looking for. Remove the `py/bin` components and add `.py` to
+# the end. e.g., the implementation of `py/bin/llvm_tools/get_patch` is in
+# `llvm_tools/get_patch.py`.
+#
 # Intended to be the symlink target for tools in `../py/bin`.
 #
 # This invokes `../venv_python3_wrapper.py` after ensuring that the appropriate
--- venvless_python3_wrapper.py
+++ venvless_python3_wrapper.py
@@ -2,6 +2,11 @@
 # Copyright 2025 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+#
+# **NOTE**: If you opened a file in py/bin/... and got here, this is unlikely
+# the droid you're looking for. Remove the `py/bin` components and add `.py` to
+# the end. e.g., the implementation of `py/bin/llvm_tools/get_patch` is in
+# `llvm_tools/get_patch.py`.
 
 """Wrapper for 'venvless' Python scripts in toolchain-utils.
 
